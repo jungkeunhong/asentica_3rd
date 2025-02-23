29b3c15ed3585b2f5f784148f94e6a9fd708f086
0 <jungkeun_hong@berkeley.edu>
2025-02-23 13:59:51 -0800

    Image update

--- .history/src/components/FeaturedTreatments_20250223135839.tsx
+++ .history/src/components/FeaturedTreatments_20250223135839.tsx
@@ -5,7 +5,7 @@ const treatments = [
   {
     id: 1,
     title: 'Jawline Contouring Injections',
-    image: 'https://images.unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%9D%98%EC%82%AC%EC%97%90%EA%B2%8E-%EC%B9%98%EC%95%84-%EA%B2%80%EC%82%AC%EB%A5%BC-%EB%B0%9B%EB%8A%94-%EC%97%AC%EC%84%B1-5mUpfUhwkvg',
+    image: 'https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%9D%98%EC%82%AC%EC%97%90%EA%B2%8E-%EC%B9%98%EC%95%84-%EA%B2%80%EC%82%AC%EB%A5%BC-%EB%B0%9B%EB%8A%94-%EC%97%AC%EC%84%B1-5mUpfUhwkvg',
     description: 'Achieve a sharp, defined jawline with our contouring solutions'
   },
   {
@@ -17,13 +17,13 @@ const treatments = [
   {
     id: 3,
     title: 'Skin Resurfacing Peel',
-    image: 'https://images.unsplash.com/photo-1598300188904-6287d52746ad?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%9D%98%EC%82%AC%EB%A1%9C%EB%B6%80%ED%84%B0-%EB%B3%B4%ED%8B%B8%EB%9D%BC%EC%A7%80-%EC%B9%98%EB%A3%8C%EB%A5%BC-%EB%B0%9B%EB%8A%94-%EC%97%AC%EC%84%B1-KBzs075982A',
     description: 'Reveal smoother, more radiant skin'
   },
   {
     id: 4,
     title: 'Skin Revitalization Therapy',
-    image: 'https://images.unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%A0%84%EC%9E%90-%EB%A7%88%EC%82%AC%EC%A7%80-%EC%96%BC%EA%B5%B4%EC%9D%84-%EA%B0%80%EC%A7%84-%EC%A0%8A%EC%9D%80-%EC%97%AC%EC%9E%90-fSJ1HrH_Vfk',
+    image: 'https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%A0%84%EC%9E%90-%EB%A7%88%EC%82%AC%EC%A7%80-%EC%96%BC%EA%B5%B4%EC%9D%84-%EA%B0%80%EC%A7%84-%EC%A0%8A%EC%9D%80-%EC%97%AC%EC%9E%90-fSJ1HrH_Vfk',
     description: 'Restore youthful elasticity and glow'
   }
 ];
