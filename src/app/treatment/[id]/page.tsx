11e5ffce934d1f0241cd3c4a1ff99ce1cdd14388
0 <jungkeun_hong@berkeley.edu>
2025-02-23 21:05:03 -0800

    Treatment card

--- src/app/treatment/[id]/page.tsx
+++ src/app/treatment/[id]/page.tsx
@@ -11,7 +11,9 @@ export default function TreatmentPage() {
 
   useEffect(() => {
     if (params?.id) {
-      setTreatmentId(params.id);
+      // params.id가 배열인 경우 첫 번째 값을 사용하고, 문자열인 경우 그대로 사용
+      const id = Array.isArray(params.id) ? params.id[0] : params.id;
+      setTreatmentId(id);
     }
   }, [params]);
 
