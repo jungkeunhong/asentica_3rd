698554d01d55704f0be2d1034034b9f998924c3a
0 <jungkeun_hong@berkeley.edu>
2025-03-01 01:07:22 -0800

    Enhance login

--- .history/src/components/Navbar_20250301010533.tsx
+++ .history/src/components/Navbar_20250301010533.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
     return () => {
       subscription.unsubscribe();
     };
-  }, [supabase.auth]);
+  }, []);
 
   const handleLoginSuccess = () => {
     setIsAuthenticated(true);
