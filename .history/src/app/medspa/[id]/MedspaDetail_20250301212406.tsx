08bb2cd08fbc5c99b88fc349a7b66a544ac23a80
0 <jungkeun_hong@berkeley.edu>
2025-03-01 21:26:06 -0800

    Change image slide

--- .history/src/app/medspa/[id]/MedspaDetail_20250301212406.tsx
+++ .history/src/app/medspa/[id]/MedspaDetail_20250301212406.tsx
@@ -305,7 +305,7 @@ export default function MedspaDetail({ medspa }: MedspaDetailProps) {
                 <path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/>
               </svg>
             </div>
-            <h3 className="text-lg font-medium text-red-700 font-sans">Cons</h3>
+            <h3 className="text-lg font-medium text-red-700">Cons</h3>
           </div>
           <ul className="space-y-3">
             {badReviews.map((review, index) => (
